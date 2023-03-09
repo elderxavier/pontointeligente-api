67393ae58f55365fbbbe32ec89531ce7a746a7ee
Elder Xavier <eldersxavier@gmail.com>
2023-03-09 13:56:35 -0300

    commit inicial do projeto

---
+++ src/main/java/com/elderxavier/pontointeligente/api/PomtointeligenteApplication.java
@@ -0,0 +1,13 @@
+package com.elderxavier.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomtointeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomtointeligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elderxavier/pontointeligente/api/PomtointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elderxavier.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomtointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
